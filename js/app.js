f2aa6f88b7216e9513d6cbc9e545dcff7f0cd2d0
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-01-30 17:19:51 +0330

    create new Todo

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 import { addTodo , completeTodo , removeTodo } from "../Redux/ActionType.js"
 
+import { addTodoAction } from "../Redux/ActionCreator.js"
+
+const addTodoBtn = document.querySelector(".add_todo_btn")
+const completeTodoBtn = document.querySelector(".todo_complete")
+const removeTodoBtn = document.querySelector(".todo_delete")
+
+const todoTitle = document.querySelector('.add_todo_input')
+
 // create reducer
 
 function todoReducer (state=[], action){
     switch(action.type){
         case addTodo:{
-            return state
+            let newState = [...state]
+            let newObjTodo = {
+                id:crypto.randomUUID(),
+                title:action.title,
+                isCompleted:false 
+            }
+            newState.push(newObjTodo)
+            return newState
         }
         case completeTodo:{
             return state
@@ -21,4 +36,14 @@ function todoReducer (state=[], action){
 
 // create Redux store
 const store = Redux.createStore(todoReducer)
-console.log(store);
+
+
+
+// add todo 
+addTodoBtn.addEventListener('click' , event=>{
+    event.preventDefault()
+    const newTodoTitle = todoTitle.value.trim() 
+    store.dispatch(addTodoAction(newTodoTitle))
+    const todos = store.getState()
+    todoTitle.value = ""
+})
